6f039a9f507f11713a8c970c8f152a64ef9f809b
Braulio <braulio.diez@gmail.com>
2018-03-31 15:22:31 +0200

    inecjting background once react is up and running

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -4,7 +4,29 @@ import { HomeRoute } from './pages/home-page';
 import { SearchRoute } from './pages/search-page';
 import { DetailRoute } from './pages/detail-page';
 
+export class AppRouter extends React.Component {
 
+  public componentDidMount() {
+    // We just want to display the background image once all the app is ready
+    // if not it just doesn't display well
+    const nonTypedBody : any = document.getElementsByTagName("BODY")[0];    
+    nonTypedBody.style.background = 'url("../assets/img/bg.jpg")';
+  }
+
+  public render() {
+    return(
+      <BrowserRouter>
+      <Switch>
+        {HomeRoute}
+        {SearchRoute}
+        {DetailRoute}
+      </Switch>
+    </BrowserRouter>  
+    );
+  }
+}
+
+/*
 export const AppRouter = () => (
   <BrowserRouter>
     <Switch>
@@ -14,3 +36,4 @@ export const AppRouter = () => (
     </Switch>
   </BrowserRouter>
 );
+*/
